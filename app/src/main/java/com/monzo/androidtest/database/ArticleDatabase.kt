217ba1a4c7a1e0199b01d70c6ddf4c2253aa4956
Faris Zaman <fariszaman@hotmail.co.uk>
2021-05-01 10:20:07 +0100

    Clean up old DAO methods

--- app/src/main/java/com/monzo/androidtest/database/ArticleDatabase.kt
+++ app/src/main/java/com/monzo/androidtest/database/ArticleDatabase.kt
@@ -20,12 +20,6 @@ interface ArticleDao {
     @Query("SELECT * FROM article_table WHERE (title LIKE '%' || :query || '%') AND (favourite == :favourites) ORDER BY published DESC LIMIT 30")
     fun getArticlesByQuery(query: String, favourites: Boolean): Flow<List<DBArticle>>
 
-    @Query("SELECT * FROM article_table ORDER BY published DESC")
-    fun getArticles(): Flow<List<DBArticle>>
-
-    @Query("SELECT * FROM article_table WHERE (title LIKE '%' || :query || '%') AND (favourite == 1) ORDER BY published DESC LIMIT 5")
-    fun getFavouriteArticles(query: String?): Flow<List<DBArticle>>
-
     @Query("SELECT * FROM article_table WHERE id == :articleId")
     fun getSingleArticle(articleId: String): Flow<DBArticle>
 }
